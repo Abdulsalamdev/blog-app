96e87fdc9a60a3e70874aa5f3bfcab196739b0fc
Abdulsalam Akinyoola <abdulsalamakinyoola@gmail.com>
2025-06-17 14:09:43 +0100

    feat: A new feature added

---
+++ src/utils/fetchArticle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const fetchArticle = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default fetchArticle
---
+++ src/utils/getDescription.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const getDescription = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default getDescription
